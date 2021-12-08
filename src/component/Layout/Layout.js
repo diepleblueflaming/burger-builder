885e970b4eb4c3242e89dbf8bebdb305eb2bbd12
dieple <lhdiep95@gmail.com>
2021-12-08 09:16:43 +0700

    [feat] commit last change.

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -3,20 +3,44 @@
  * Author: Le Hai Diep(dieple)
  * Date-Time: 14/10/2019-22:33
  */
-import React from 'react';
+import React, {Component} from 'react';
 import {Toolbar} from "../Navigation/Toolbar/Toolbar";
 import {SideDrawer} from "../Navigation/SideDrawer/SideDrawer";
 
-export const Layout = (props) => {
-  return (
-    <div className="Layout">
-      <Toolbar/>
-      <main>
-        {props.children}
-      </main>
-      <nav>
-        <SideDrawer/>
-      </nav>
-    </div>
-  );
+export class Layout extends Component {
+
+  state = {
+    showSideDrawer: false
+  };
+
+  sideDrawerCloseHandler = () => {
+    this.setState({
+      showSideDrawer: false
+    });
+  };
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return {
+        showSideDrawer: !prevState.showSideDrawer
+      }
+    });
+  };
+
+  render () {
+    return (
+      <div className="Layout">
+        <Toolbar SideDrawerToggleClicked={this.sideDrawerToggleHandler}/>
+        <main>
+          {this.props.children}
+        </main>
+        <nav>
+          <SideDrawer
+            open={this.state.showSideDrawer}
+            sideDrawerCloseHandler={this.sideDrawerCloseHandler}
+          />
+        </nav>
+      </div>
+    );
+  }
 };
